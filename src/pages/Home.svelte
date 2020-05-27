073600982e4357a7faec08af634bd68210e50969
dsbrgg <dsbrgg@gmail.com>
2020-05-27 17:53:56 +0100

    update Home.svelte

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,8 +2,7 @@
   export let location;
 </script>
 
-<h1>My home</h1>
-<h1>My home</h1>
-<h1>My home</h1>
-<h1>My home</h1>
-<h1>My home</h1>
+<h1>Hello World!</h1>
+
+<p>This is my personal website which won't have too many things.</p>
+<p>You can quickly select from the tab above the section that you're interested.</p>
