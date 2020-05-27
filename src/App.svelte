1cbe9439442ca87e5af1071dff535c6b3e374574
dsbrgg <dsbrgg@gmail.com>
2020-05-27 17:53:33 +0100

    update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,10 @@
 <script>
-  import { Router, Link, Route } from "svelte-routing";
-  import Home from "pages/Home.svelte";
-  import Resume from "pages/Resume.svelte";
-  import Blog from "pages/Blog.svelte";
+  import { Router } from "svelte-routing";
+  import Content from 'containers/content/Content.svelte';
   import Background from 'containers/background/Background.svelte';
 </script>
 
 <Router>
   <Background />
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="blog">Blog</Link>
-    <Link to="resume">Resume</Link>
-  </nav>
-  <div>
-    <Route path="/" component="{Home}" />
-    <Route path="blog" component="{Blog}" />
-    <Route path="resume" component="{Resume}" />
-  </div>
+  <Content /> 
 </Router>
