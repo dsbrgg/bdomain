d34a6599f2fcaa07094f14cde20605c4ea0daa47
dsbrgg <dsbrgg@gmail.com>
2020-05-28 22:43:14 +0100

    update Experience.svelte

--- src/pages/resume/components/Experience.svelte
+++ src/pages/resume/components/Experience.svelte
@@ -1,6 +1,39 @@
 <script>
 </script>
 
-<div>
-  <h1>So dumb :(</h1>
+<style>
+  .experience-container {
+    display: flex;
+    margin-left: 4em;
+    margin-right: 4em;
+    margin-bottom: 4em;
+  }
+
+  .experience-general {
+    display: flex;
+    border-bottom: #000000 solid 2px;
+    border-right: #000000 solid 2px;
+    box-shadow: 1px 2px 10px 1px;
+    flex-direction: column;
+    padding: 1em;
+  }
+
+  .experience-description {
+    align-self: center;
+    margin-left: 4em;
+    border-bottom: #000000 solid 1px;
+    padding-bottom: 1em;
+  }
+</style>
+
+<div class="experience-container">
+  <div class="experience-general">
+    <h3>Seegno</h3>
+    <span>Backend Engineer</span>
+    <span>Oct|2019 - Present</span>
+  </div> 
+  
+  <span class="experience-description">
+    Huge description to make sure you understand I'm great and you should really hire me.
+  </span>
 </div>
