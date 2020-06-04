1e7ce7a00b7254783de3253c52cb1ef941a346b8
dsbrgg <dsbrgg@gmail.com>
2020-06-04 18:52:30 +0100

    Add Skills.svelte

---
+++ src/pages/resume/components/Skills.svelte
@@ -0,0 +1,42 @@
+<script>
+</script>
+
+<style>
+  .skills-title {
+    display: inline-block;
+    font-weight: bold;
+    font-style: italic;
+    font-size: 1.5em;
+    margin-top: 0.7em;
+    padding-bottom: 0.5em;
+    margin-bottom: 0.5em;
+    border-bottom: #000000 solid 1px;
+    color: darkgoldenrod;
+  }
+
+  .skills-container {
+    display: flex;
+    margin-top: 2em; 
+    margin-left: 4em;
+    margin-right: 4em;
+    margin-bottom: 4em;
+  }
+
+  @media (max-width: 500px), (max-height: 800px) {
+    .skills-container {
+      display: flex;
+      flex-direction: column;
+      margin-left: 4em;
+      margin-right: 4em;
+      margin-bottom: 4em;
+    }
+  }
+</style>
+
+<span class="skills-title">Skills</span>
+<div class="skills-container">
+  <ul>
+    <li>Javascript: OK</li>
+  </ul>
+</div>
+<hr />
