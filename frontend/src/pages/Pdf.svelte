964820e1df922e4ca124281c0461f6d2c22f53b6
dsbrgg <dsbrgg@gmail.com>
2020-06-12 18:23:16 +0100

    Add Pdf.svelte

---
+++ frontend/src/pages/Pdf.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Resume from 'pages/resume/Resume.svelte';
+</script>
+
+<style>
+  #pdf-page {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 99999999;
+    background-color: #ffffff;
+    text-align: center;
+  }
+
+  #pdf-format {
+    display: flex;
+    flex-direction: column;
+    margin-left: 10%;
+    margin-right: 10%;
+  }
+</style>
+
+<div id="pdf-page">
+  <div id="pdf-format">
+    <Resume />
+  </div>
+</div>
