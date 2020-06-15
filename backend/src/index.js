6ed0405da481bdbd3b393df5760bde6c4192831d
dsbrgg <dsbrgg@gmail.com>
2020-06-15 14:13:44 +0100

    update backend index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,20 @@
 'use strict';
 
 const Koa = require('koa');
+const Router = require('koa-router');
+const send = require('koa-send');
 const config = require('config');
 const puppeteer = require('puppeteer');
 
 const app = new Koa();
+const router = new Router();
 
 const { port } = config.get('server');
 const { format, url, parameters } = config.get('pdf');
 
+
 // https://blog.risingstack.com/pdf-from-html-node-js-puppeteer/
-app.use(async ctx => {
+router.get('/page', async ctx => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(url, parameters);
@@ -18,8 +22,33 @@ app.use(async ctx => {
 
   await browser.close();
 
+  ctx.set('Content-Disposition', 'attachment; filename=diego_braga_cv.pdf');
   ctx.set('Content-Type', 'application/pdf');
   ctx.body = pdf;
+});
+
+router.get('/', async ctx => {
+  await send(ctx, ctx.path, { root: `${__dirname}/public/index.html` });
+});
+
+router.get('/resume', async ctx => {
+  await send(ctx, '/', { root: `${__dirname}/public/index.html` });
+});
+
+router.get('/blog', async ctx => {
+  await send(ctx, '/', { root: `${__dirname}/public/index.html` });
+});
+
+router.get('/pdf', async ctx => {
+  await send(ctx, '/', { root: `${__dirname}/public/index.html` });
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.use(async ctx => {
+  await send(ctx, ctx.path, { root: `${__dirname}/public` });
 });
 
 app.listen(5001);
