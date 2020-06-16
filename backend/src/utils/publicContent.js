6abd6e584e90e000e80f64bbb033c00edfcf4c58
dsbrgg <dsbrgg@gmail.com>
2020-06-16 16:53:23 +0100

    add backend general utils

---
+++ backend/src/utils/publicContent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const { join } = require('path');
+const send = require('koa-send');
+
+module.exports = async ctx => {
+  const src = join(__dirname, '..', '..', 'src');
+  await send(ctx, '/', { root: `${src}/public/index.html` });
+};
