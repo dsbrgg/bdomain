f30c1a3f3a57dca3d387b9cc5d9fd6e376a119f2
dsbrgg <dsbrgg@gmail.com>
2020-06-17 17:25:39 +0100

    Update utils publicContent

--- backend/src/routes/utils/publicContent.js
+++ backend/src/routes/utils/publicContent.js
@@ -4,6 +4,10 @@ const { join } = require('path');
 const send = require('koa-send');
 
 module.exports = async ctx => {
-  const src = join(__dirname, '..', '..', 'src');
+  const split = __dirname.split('/');
+  const index = split.indexOf('src');
+  const path = split.slice(0 , index).join('/');
+  const src = join(path, 'src');
+
   await send(ctx, '/', { root: `${src}/public/index.html` });
 };
