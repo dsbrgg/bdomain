527f2fe1fc720009992a38448e07b448555c56e4
dsbrgg <dsbrgg@gmail.com>
2020-06-17 17:24:51 +0100

    Rename src/routes/webpage/index to src/routes/webpage/webpage

--- backend/src/routes/webpage/webpage.js
+++ backend/src/routes/webpage/webpage.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const KoaRouter = require('koa-router');
-const publicContent = require('../../utils/publicContent');
+
+const blog = require('./blog/blog');
+const resume = require('./resume/resume');
 const prepareRouter = require('../utils/prepareRouter');
+const publicContent = require('./../utils/publicContent');
 
 const router = new KoaRouter({});
 
 router.get('/', publicContent);
-router.get('/resume', publicContent);
-router.get('/blog', publicContent);
+router.use('/blog', blog.routes());
+router.use('/resume', resume.routes());
 
 module.exports = prepareRouter(router);
