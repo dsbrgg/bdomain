d0cd5d855372df73cd1a3ba02342a05d436c6ee2
dsbrgg <dsbrgg@gmail.com>
2020-07-03 21:54:37 +0100

    update server renderTemplate

--- src/server/routes/renderTemplate.js
+++ src/server/routes/renderTemplate.js
@@ -1,21 +1,32 @@
 'use strict';
 
+import config from 'config';
 import template from 'template';
 import ssr from 'public/build/ssr.js';
 import resume from 'routes/webpage/resume/resume.json';
 
+const client = config.get('client');
+
 const getState = url => {
   switch (url) {
-    case '/': return { ...resume, url };
-    case '/blog': return { ...resume, url };
-    case '/resume': return { ...resume, url };
-    case '/pdf': return { ...resume, url };
+    case '/': return { ...resume, client, url };
+    case '/blog': return { client, url };
+    case '/resume': return { ...resume, client, url };
+    case '/pdf': return { ...resume, client, url };
+    default: return null;
   }
 };
 
 export default async ctx => {
   const state = getState(ctx.url);
+  const mountState = ctx.get('X-State');
   const { html, css, head } = ssr.render(state);
+
+  if (mountState === ctx.url) {
+    ctx.body = state;
+
+    return;
+  }
 
   ctx.set('Content-Type', 'text/html');
   ctx.body = template({ css, head, html, state });
