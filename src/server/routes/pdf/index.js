da24d17db44fd34d013de31850d1e5439f2e0e43
dsbrgg <dsbrgg@gmail.com>
2020-07-03 17:40:10 +0100

    update server pdf routes

--- src/server/routes/pdf/index.js
+++ src/server/routes/pdf/index.js
@@ -3,11 +3,10 @@
 import KoaRouter from 'koa-router';
 import config from 'config';
 import puppeteer from 'puppeteer';
-import renderTemplate from 'routes/utils/renderTemplate';
+import renderTemplate from 'routes/renderTemplate';
 import prepareRouter from 'routes/utils/prepareRouter';
 
-const { port } = config.get('server');
-const { headless, format, url, parameters } = config.get('pdf');
+const { headless, filename, format, url, parameters } = config.get('pdf');
 const router = new KoaRouter({ prefix: '/pdf' });
 
 router.get('/', renderTemplate);
@@ -22,7 +21,7 @@ router.get('/download', async ctx => {
 
   await browser.close();
 
-  ctx.set('Content-Disposition', 'attachment; filename=diego_braga_cv.pdf');
+  ctx.set('Content-Disposition', `attachment; filename=${filename}.pdf`);
   ctx.set('Content-Type', 'application/pdf');
   ctx.body = pdf;
 });
