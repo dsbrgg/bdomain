a39a306967770efa2e13c847b26dd2c062b33f5a
dsbrgg <dsbrgg@gmail.com>
2020-07-03 17:38:16 +0100

    remove src server publicContent util

--- src/server/routes/utils/publicContent.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import send from 'koa-send';
-import publicPath from 'routes/utils/publicPath';
-
-export default async ctx => {
-  await send(ctx, '/', { root: `${publicPath}/index.html` });
-};
