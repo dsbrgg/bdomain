ba1bf980822b369e2eaa7e61ae4286a2d2856a30
dsbrgg <dsbrgg@gmail.com>
2020-07-03 21:54:12 +0100

    update config default

--- config/default.js
+++ config/default.js
@@ -1,11 +1,17 @@
 module.exports = {
+  client: {
+    api: 'http://localhost:5001'
+  },
   server: {
     port: 5001
   },
   pdf: {
     headless: true,
     filename: 'diebo_braga_cv',
-    format: 'Tabloid',
+    format: {
+      width: 1800,
+      height: 2400
+    },
     url: 'http://localhost:5001/pdf',
     parameters: {
       waitUntil: 'networkidle0'
