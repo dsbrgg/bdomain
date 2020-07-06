e1cf9b9683e785dff244a9b085b30df23bf725a1
dsbrgg <dsbrgg@gmail.com>
2020-07-06 21:30:42 +0100

    update server pdf route

--- src/server/routes/pdf/index.js
+++ src/server/routes/pdf/index.js
@@ -27,7 +27,7 @@ router.get('/download', async ctx => {
   await page.setViewport(viewport);
   await page.goto(url, parameters);
   await page.emulateMedia(media);
-  await page.waitFor(5000);
+  await page.waitFor(2000);
 
   const pdf = await page.pdf(format);
 
