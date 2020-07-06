9c845df44a7b8cc0dcba1a19d987be952007c0d3
dsbrgg <dsbrgg@gmail.com>
2020-07-06 09:33:21 +0100

    update server template

--- src/server/template/index.js
+++ src/server/template/index.js
@@ -11,12 +11,13 @@ export default ({ html, css, head, state }) => `
       <link rel='icon' type='image/png' href='/favicon.png'>
       <link rel='stylesheet' href='/normalize.css'>
       <link rel='stylesheet' href='/build/bundle.css'>
-      <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Caveat:wght@400;700&family=Permanent+Marker&family=Shadows+Into+Light&family=Roboto+Mono:ital,wght@0,100;0,400;0,700;1,100&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Caveat:wght@400;700&family=Permanent+Marker&family=Shadows+Into+Light&family=Roboto+Mono:ital,wght@0,100;0,400;0,700;1,100&family=Fira+Code:wght@700&display=swap" rel="stylesheet">
     </head>
     <body>
       ${html}
       <script src="https://kit.fontawesome.com/5063aa095a.js" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/p5@1.0.0/lib/p5.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/downloadjs/1.4.8/download.min.js"></script>
       <script>
         window.__PRELOADED_STATE__ = ${JSON.stringify(state).replace(
           /</g,
