afa2ac7a3418f0ae560b507f98c41486fc515bd4
dsbrgg <dsbrgg@gmail.com>
2020-07-08 18:52:28 +0100

    update Home.svelte

--- src/client/pages/Home.svelte
+++ src/client/pages/Home.svelte
@@ -1,11 +1,64 @@
 <script>
+  import { getContext, onMount } from 'svelte';
+  import fetchState from 'api/index.js';
+  import Transition from 'components/Transition.svelte';
+  import Loading from 'components/Loading.svelte';
+
   export let location;
+
+  let data = {
+    init: false,
+    greetings: undefined,
+    text: undefined
+  };
+
+  const store = getContext('initialState');
+
+  const mount = async () => {
+    const { client, home } = $store;
+
+    if (!home) { 
+      const state = '';
+      const response = await fetchState({
+        client,
+        state 
+      });
+
+      $store = { ...$store, ...response }; 
+    }
+
+    data = { ...$store.home, init: true };
+  };
+  
+  onMount(mount);
 </script>
 
-<div>
-  <h1>Hello World!</h1>
+<style>
+  .home-container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 
-  <p>Thanks for visiting my webpage.</p>
-  <p>You can checkout my resume through the tab above.</p>
-  <p>Have a good one! :)</p>
-</div>
+  .home-title {
+    margin-bottom: 3em;
+  }
+
+  .home-text {
+    margin-right: 12em;
+    margin-left: 12em;
+  }
+</style>
+
+<Transition>
+  <div class="home-container">
+    <Loading condition={data.init}>
+      <h1 class="home-title">{data.greetings}</h1>
+
+      <div class="home-text">
+        <p>{data.text}</p>
+      </div>
+    </Loading>
+  </div>
+</Transition>
